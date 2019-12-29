bc4daa6785f4b528b01456a025d6190d172383bf
Tushar <tusharbarman123@gmail.com>
2019-12-29 23:29:21 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("Project1"),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/tired.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
